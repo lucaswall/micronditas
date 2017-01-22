2a512c2dc8c24e7b6e511d2da78bcf4346fa3408
Lucas Wall <wall.lucas@gmail.com>
2017-01-21 21:33:09 -0300

    First commit.

---
+++ UnityProject/Assets/Scripts/Microwave.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Microwave : MonoBehaviour {
+
+	public float initialCookPoints;
+	public float timeTick;
+	public float burnEnergy;
+	public float gainPerAction;
+	public float decayPerSec;
+	public float roundTime;
+
+	public Image cookBar;
+	public Image energyBar;
+	public Text remTimeText;
+	public MicrowaveVisual visual;
+
+	float cookPoints;
+	float energyLevel;
+	float time;
+
+	float nextTick;
+	bool stopped = false;
+
+	void Start() {
+		InitSimulation();
+		nextTick = timeTick;
+	}
+
+	void Update() {
+		if ( stopped ) {
+			if ( Input.GetKeyDown(KeyCode.R) ) {
+				InitSimulation();
+				nextTick = timeTick;
+				stopped = false;
+			}
+			return;
+		}
+		nextTick -= Time.deltaTime;
+		if ( nextTick <= 0.0f ) {
+			nextTick += timeTick;
+			RunTick();
+		}
+		energyLevel -= decayPerSec * Time.deltaTime;
+		time -= Time.deltaTime;
+		if ( time <= 0.0f ) {
+			LostLevel();
+		}
+		if ( Input.GetKeyDown(KeyCode.Space) ) {
+			AddEnergy();
+		}
+		UpdateVisuals();
+	}
+
+	void InitSimulation() {
+		cookPoints = initialCookPoints;
+		energyLevel = 0.0f;
+		time = roundTime;
+	}
+
+	void RunTick() {
+		cookPoints -= energyLevel;
+		if ( cookPoints <= 0.0f ) {
+			WonLevel();
+		}
+	}
+
+	void AddEnergy() {
+		energyLevel += gainPerAction;
+		if ( energyLevel > burnEnergy ) {
+			LostLevel();
+		}
+	}
+
+	void WonLevel() {
+		Debug.Log("WON LEVEL!!!");
+		stopped = true;
+	}
+
+	void LostLevel() {
+		Debug.Log("LOST LEVEL!!!!");
+		stopped = true;
+	}
+
+	void UpdateVisuals() {
+		cookBar.fillAmount = cookPoints / initialCookPoints;
+		energyBar.fillAmount = energyLevel / burnEnergy;
+		remTimeText.text = time.ToString();
+		visual.SetTime(time);
+		visual.SetEnergyLevel(energyLevel / burnEnergy);
+	}
+
+}
+
---
+++ UnityProject/Assets/Scripts/MicrowaveVisual.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MicrowaveVisual : MonoBehaviour {
+
+	public TextMesh timerText;
+	public SpriteRenderer windowSpr;
+	public Color energyMin, energyMax;
+
+	public void SetTime(float t) {
+		int min = Mathf.FloorToInt(t / 60.0f);
+		int sec = Mathf.CeilToInt(t) % 60;
+		timerText.text = String.Format("{0:D2}:{1:D2}", min, sec);
+	}
+
+	public void SetEnergyLevel(float level) {
+		windowSpr.color = Color.Lerp(energyMin, energyMax, level);
+	}
+
+}
